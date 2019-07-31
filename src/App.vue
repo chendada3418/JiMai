43eddfbd3d16c4f03afc9d8482df4f36610b351b
chendada3418 <cjx3418@qq.com>
2019-08-01 04:31:54 +0800

    上传配置文件

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <span>父组件</span><br>
+    {{abc}}
+    <hr>
+    <h3>测试</h3>
+    <h3>66666666666</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+     abc:'测试文件'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
